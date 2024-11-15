53c22933538963cfdbaa1ab14a9ba4106f101909
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-15 23:09:31 +0100

    Create button click listeners

--- app/src/main/java/dev/dlovan/maffpuff/MainActivity.kt
+++ app/src/main/java/dev/dlovan/maffpuff/MainActivity.kt
@@ -1,20 +1,45 @@
 package dev.dlovan.maffpuff
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dev.dlovan.maffpuff.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.btnAddition.setOnClickListener {
+            val newIntent = Intent(this, MathActivity::class.java)
+            newIntent.putExtra("operation", "addition")
+        }
+        binding.btnSubtraction.setOnClickListener {
+            val newIntent = Intent(this, MathActivity::class.java)
+            newIntent.putExtra("operation", "subtraction")
+        }
+        binding.btnMultiplication.setOnClickListener {
+            val newIntent = Intent(this, MathActivity::class.java)
+            newIntent.putExtra("operation", "multiplication")
+        }
+        binding.btnDivision.setOnClickListener {
+            val newIntent = Intent(this, MathActivity::class.java)
+            newIntent.putExtra("operation", "division")
+        }
+
     }
 }
